b29b53926df0b2aa4d97d9b9253bc1b4c6349422
Alexander <alexander@teamcorgi.com>
2017-04-11 03:31:26 +0200

    lolk

---
+++ src/calculate/Edges.java
@@ -0,0 +1,10 @@
+package calculate;
+
+/**
+ * Created by jasve_29 on 11-Apr-17.
+ */
+public enum Edges {
+    LeftEdge,
+    RightEdge,
+    BottomEdge
+}
--- src/calculate/KochManager.java
+++ src/calculate/KochManager.java
@@ -1,56 +1,66 @@
 package calculate;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import jsf31kochfractalfx.JSF31KochFractalFX;
 import timeutil.TimeStamp;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.*;
 
 /**
  * Created by jasve_29 on 21-Mar-17.
  */
-public class KochManager implements Observer {
+public class KochManager {
 
     private JSF31KochFractalFX application;
-    private KochFractal koch;
-    private List<Edge> edges = new ArrayList<>();
+    public List<Edge> edges = Collections.synchronizedList(new ArrayList<Edge>());
+    TimeStamp drawTimer = new TimeStamp();
+    TimeStamp updateTimer = new TimeStamp();
+    public int threadCount = 0;
 
     public KochManager(JSF31KochFractalFX application) {
 
         this.application = application;
-        koch = new KochFractal();
-        koch.addObserver(this);
     }
 
-    @Override
-    public void update(Observable o, Object arg) {
-     //   application.drawEdge((Edge)arg);
-        edges.add((Edge)arg);
-    }
+//    @Override
+//    public void update(Observable o, Object arg) {
+//     //   application.drawEdge((Edge)arg);
+//        edges.add((Edge)arg);
+//
+//    }
 
     public void changeLevel(int currentLevel) {
-        koch.setLevel(currentLevel);
         edges.clear();
-        drawEdges();
+        updateTimer.init();
+        updateTimer.setBegin();
+        new Thread(new KochRunnable(currentLevel, this, Edges.LeftEdge)).start();
+        new Thread(new KochRunnable(currentLevel, this, Edges.RightEdge)).start();
+        new Thread(new KochRunnable(currentLevel, this, Edges.BottomEdge)).start();
+
     }
 
     public void drawEdges() {
-        TimeStamp t = new TimeStamp();
-        t.setBegin("Clearing panel");
+        drawTimer.init();
+        drawTimer.setBegin();
         application.clearKochPanel();
-        t.setEndBegin("Generating edges...");
-        koch.generateLeftEdge();
-        koch.generateBottomEdge();
-        koch.generateRightEdge();
-        t.setEndBegin("drawing..");
-        for(Edge e : edges) {
-            application.drawEdge(e);
+        synchronized (edges) {
+            for (Edge e : edges) {
+                application.drawEdge(e);
+            }
         }
-        t.setEnd("Finished drawing.");
-        application.setTextCalc(t.toString());
-        application.setTextNrEdges(Integer.toString(edges.size()));
+        drawTimer.setEnd();
+        application.setTextDraw(drawTimer.toString());
+    }
+
+    public void threadFinished() {
+        if (threadCount%3 == 0) {
+                updateTimer.setEnd();
+            Platform.runLater(() -> application.setTextCalc(updateTimer.toString()));
+            Platform.runLater(() -> application.setTextNrEdges(Integer.toString(edges.size())));
+          //  drawEdges();
+            application.requestDrawEdges();
+        }
     }
 }
+
